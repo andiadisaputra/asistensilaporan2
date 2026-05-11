7c44676cc6abb4e6960710d295936fd163598ef9
andiadisaputra <andiadisaputra012@gmail.com>
2026-05-11 21:20:00 +0700

    Berhasil membuat manajement clinic

---
+++ clinic-api/app/Models/Doctor.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Doctor extends Model
+{
+protected $fillable = ['name', 'phone', 'license_number', 'specialist_id'];
+
+public function specialist()
+{
+    return $this->belongsTo(Specialist::class);
+}
+}
